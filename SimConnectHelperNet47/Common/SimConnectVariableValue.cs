21fa94482370b2a15d45af618a9e5cf68d86ce08
Ondřej Kotas <ok@krtkovo.eu>
2022-09-17 10:12:22 +0200

    SimConnectHelper for .net 4.7

---
+++ SimConnectHelperNet47/Common/SimConnectVariableValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaxOnBoard.Helpers.Common
+{
+    public class SimConnectVariableValue
+    {
+        public SimConnectVariable Request { get; set; }
+        public object Value { get; set; }
+    }
+}
---
+++ SimConnectHelperNet47/Common/SimVarTimer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaxOnBoard.Helpers.Common
+{
+    public class SimVarTimer
+    {
+        public int RequestID { get; set; }
+        public int TimerID { get; internal set; }
+        public SimConnectVariable Request { get; set; }
+        public int FrequencyInMs { get; set; }
+    }
+}
